d4a4e3ce763219882700ef7336a685cb183f54cb
ldvinh <ldvinh@hotmail.com>
2025-08-02 00:40:30 +0700

    update project

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,65 +1,38 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-1", "Optimistic Chubby", "img/projects/project-1-icon.png", 
+    new ProjectData("project-1", "Heroes & Empire: NFT", "img/projects/he/he4.png", 
     `
     <div class="paragraph">
-     <strong>Optimistic Chubby</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+     <strong>Heroes & Empires is an NFT-integrated idle RPG</strong> strategy game developed by Imba Studio. Featuring collectible and tradeable NFT heroes, the game combines deep strategic gameplay with modern blockchain technology. It proudly <strong> won First Prize at the Game2Blockchain 2021 </strong> competition, hosted by Axie Infinity, Kyber Network, and TomoChain.   
+     </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/fuEYSrGwKHU" frameborder="0" allowfullscreen></iframe>
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
-    </div>
-
-    <div class="paragraph">
-        Main features :
-        <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-        </ul>
+        <a href="https://play.google.com/store/apps/details?id=co.imba.he" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
     </div>
 
     <div class="paragraph center">
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
+        <img class="phone-screenshot" src="img/projects/he/he1.png" alt="" />
+        <img class="phone-screenshot" src="img/projects/he/he2.png" alt="" />
+        <img class="phone-screenshot" src="img/projects/he/he5.png" alt="" />
+        <img class="phone-screenshot" src="img/projects/he/he6.png" alt="" />
     </div>
     `, "#23bd69", true),
-    new ProjectData("project-2", "Singing Addict", "img/projects/project-2-icon.png", `
+    new ProjectData("project-2", "Heroes Awaken: Idle RPG", "img/projects/ha/ha7.png", `
     <div class="paragraph">
-        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
+        <strong>Heroes Awaken: Idle RPG</strong> is an auto-battler game by Imba Games, featuring 70+ collectible heroes and offline progression. The game <strong> won 5 awards at Vietnam Game Awards 2024</strong>, recognizing its quality and innovation in the local indie scene.
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
-
-    <div class="paragraph">
-        Main features :
-        <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-        </ul>
-    </div>
-
-    <div class="paragraph">
-    <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-    </div>
+        <iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F2269415190115213%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
     </div>
 
     <div class="paragraph center">
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+    <img class="pc-screenshot" src="img/projects/ha/ha1.png" alt="" />
+    <img class="pc-screenshot" src="img/projects/ha/ha2.png" alt="" />
+    <img class="pc-screenshot" src="img/projects/ha/ha3.png" alt="" />
+    <img class="pc-screenshot" src="img/projects/ha/ha4.png" alt="" />
     </div>
     `, "#5a78af"),
     new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
